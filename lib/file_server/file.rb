86b95e44cc6b0fa22b7b0d4c97717eae241f186c
Peter Wu <peter.wu@xbridge.com>
2012-12-05 12:45:23 +0000

    using :path as primary key instead of id

--- lib/file_server/file.rb
+++ lib/file_server/file.rb
@@ -4,10 +4,7 @@ module FileServer
 
     self.format = :json
     self.site = "http://localhost:1234/"
-
-    def self.download path
-      self.get "download", path: path
-    end
+    self.primary_key = :path
 
   end
 
